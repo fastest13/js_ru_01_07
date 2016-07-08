ff8b3ae9246a18759ea1f5e2eb4c4a0051fb1ead
fastest13 <fastest13@mail.ru>
2016-07-08 19:20:02 +0300

    CommentList --> Comment && add new CommentList component after comments

--- src/Article.js
+++ src/Article.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import CommentList from './CommentList'
 
+
 class Article extends Component {
     state = {
         isOpen: false,
-        //У тебя CommentList на самом деле Comment, лучше было создать еще один компонент для списка комментов и вынести туда эту логику
-        isCommentsOpen: false
     }
 
 /*
@@ -19,20 +18,15 @@ class Article extends Component {
 
     render() {
         const { article } = this.props;
-        const { isOpen, isCommentsOpen } = this.state
+        const { isOpen} = this.state
         const body = isOpen ? <section>{ article.text }</section> : null
-        const comments = (typeof (article.comments) !== 'undefined') ? article.comments : [];
-        const commentsButton = (comments.length)? <a href="#" onClick = {this.commentsOpen}>Коментарии ({ comments.length })</a> : null;
-        const listItems = isCommentsOpen ? comments.map((comment) => <li key = {comment.id}><CommentList comment = {comment}/></li>) : null;
+        const commentsLink = (article.comments) ? <CommentList comments = {article.comments}/> : null;
 
         return (
             <div>
                 <h1 onClick = {this.toggleOpen}>{ article.title }</h1>
                 {body}
-                {commentsButton}
-                <ul>
-                    {listItems}
-                </ul>
+                {commentsLink}
             </div>
         )
     }
@@ -42,29 +36,7 @@ class Article extends Component {
             isOpen: !this.state.isOpen
         })
     }
-
-    commentsOpen = (ev) => {
-        ev.preventDefault();
-        this.setState({
-            isCommentsOpen: !this.state.isCommentsOpen
-        })
-    }
 }
 
-
-
-/*
-function Article(props) {
-    const article = props.article
-//    const { article: { title, text } } = props
-
-    return (
-        <div>
-            <h1>{ article.title }</h1>
-            <section>{ article.text }</section>
-        </div>
-    )
-}
-*/
 
 export default Article
--- src/CommentList.js
+++ src/CommentList.js
@@ -1,13 +1,44 @@
-import React  from 'react'
+import React, { Component } from 'react'
+import Comment from './Comment'
 
-function CommentList(props) {
-    const { comment: { user, text } } = props;
+
+class CommentList extends Component {
+  state = {
+    isOpen: false
+  }
+
+  /*
+   constructor(props) {
+   super(props)
+   this.state = {
+   isCommentsOpen: false
+   }
+   }
+   */
+
+  render() {
+    const { isOpen } = this.state;
+    const comments = (typeof (this.props.comments) !== 'undefined') ? this.props.comments : [];
+    const commentsLength =  (comments.length) ? comments.length : 0;
+    const listItems = isOpen ? comments.map((comment) => <li key = {comment.id}><Comment comment = {comment}/></li>) : null;
+
     return (
-        <div>
-            <h4>{ user }</h4>
-            <p>{ text }</p>
-        </div>
+      <div>
+        <a href="#" onClick = {this.toggleOpen}>Коментарии ({ commentsLength })</a>
+        <ul>
+          { listItems }
+        </ul>
+      </div>
     )
+  }
+
+  toggleOpen = (ev) => {
+    ev.preventDefault();
+    this.setState({
+      isOpen: !this.state.isOpen
+    })
+  }
 }
+
 
 export default CommentList
